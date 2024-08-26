3daae131489b171b0984d78f3ca60118adbbf59b
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2024-08-26 15:16:12 +0200

    Cleaning up the deprecated functions

--- ChalcedonPy/Utilities.py
+++ ChalcedonPy/Utilities.py
@@ -3,188 +3,47 @@ from cycler import cycler
 from pathlib import Path # to work with paths
 import matplotlib.colors  # all related to colours
 
-def init(save_path):
+def init(save_path, display_mode):
    """Generates variables for use in the module
 
    Parameters
    ----------
    save_path : string
        The folder in which the images would be saved
-
+   display_mode: string
+       Sets the display and save type for the images
+   
    Examples
    --------
-   cp.init("Mathematical-Fundamentals")
+   cp.init(save_path="Mathematical-Fundamentals", display_mode="web")
 
    """
-   global SAVE_PATH
+   global SAVE_PATH, style
    SAVE_PATH = save_path
-   
+   style = display_mode
 
-# define a function to store matplotlib style information
-def plot_settings(style, size):
-
-   # define custom default colors for generating image with
-   # consisten color cycling
-   new_colours = ['#326199',
-                 '#4fb1a1',
-                 '#fcc055',
-                 '#eb8d50',
-                 '#df6e5b',
-                 '#9a031e',
-                 '#984ea3']
-   
-   # define the styles for generating web compatible images.
+   # Set style for use in supplemental website
    if style == "web":
-      style = {
-         'axes.edgecolor': '1e1e2e',
-         'axes.facecolor': '363a4f',
-         'axes.axisbelow' : True,
-         'axes.labelcolor' : 'cad3f5',
-         'axes.grid': True,
-         'axes.grid.which': 'both',
-         'axes.spines.left': False,
-         'axes.spines.right': False,
-         'axes.spines.top': False,
-         'axes.spines.bottom': False,
-         'axes.prop_cycle': cycler(color=new_colours),
+      plt.style.use('dusk')
 
-         'grid.color': '5b6078',
-         'grid.linewidth': '1.2',
-         
-         'xtick.color': 'cad3f5',
-         'xtick.major.bottom': True,
-         'xtick.labelsize': 10,
-         'xtick.minor.bottom': True,
-         'xtick.minor.bottom': True,
-         'xtick.minor.visible': True,
-         'xtick.minor.width': 0.5,
-         
-         'ytick.color': 'cad3f5',
-         'ytick.major.left': True,
-         'ytick.minor.left': False,
-         'ytick.minor.visible': True,
-         'ytick.labelsize': 10,
-
-         'savefig.facecolor': '363a4f',
-
-         'text.color': 'cad3f5',
-         
-         'lines.linewidth': 4,
-
-         'font.size': 16,
-         
-         'legend.fancybox' : False,
-         'legend.facecolor' : '6c7086',
-         
-         'figure.facecolor': '838ba7',
-      }
-
-      # Define the font dictionary to store label formatting
-      font = {'color':  '#cad3f5',
-              'weight': 'normal',
-              'size': 16,
-              }   
-
+   # Set style for use in ipython and lecture slide
    elif style == "slide":
-      style = {
-         'axes.edgecolor': 'f0f0f0',
-         'axes.facecolor': 'fafafa',
-         'axes.axisbelow' : True,
-         'axes.labelcolor' : '1e1e1e',
-         'axes.grid': True,
-         'axes.grid.which': 'both',
-         'axes.spines.left': False,
-         'axes.spines.right': False,
-         'axes.spines.top': False,
-         'axes.spines.bottom': False,
-         'axes.prop_cycle': cycler(color=new_colours),
-
-         'grid.color': 'f0f0f0',
-         'grid.linewidth': '1.2',
-         
-         'xtick.color': '1e1e1e',
-         'xtick.major.bottom': True,
-         'xtick.labelsize': 10,
-         'xtick.minor.bottom': True,
-         'xtick.minor.bottom': True,
-         'xtick.minor.visible': True,
-         'xtick.minor.width': 0.5,
-         
-         'ytick.color': '1e1e1e',
-         'ytick.major.left': True,
-         'ytick.minor.left': False,
-         'ytick.labelsize': 10,
-
-         'savefig.facecolor': 'fafafa',
-
-         'text.color': '1e1e1e',
-         
-         'lines.linewidth': 4,
-
-         'font.size': 12,
-         
-         'legend.fancybox' : False,
-         'legend.facecolor' : '6c7086',
-         
-         'figure.facecolor': 'fafafa',
-      }
-
-      # Define the font dictionary to store label formatting
-      font = {'color':  '#1e1e1e',
-              'weight': 'normal',
-              'size': 16,
-              }   
-
-   # Apply style sheet for use in matplotlib
-   plt.rcParams.update(style)
+      plt.style.use('dawn')
    
-   # Define figure size based on the number of figures
-   if size == 1:
-      plt.figure(figsize=(10,6))
-   elif size == 2:
-      plt.figure(figsize=(12,5))
-   elif size == None:
-      return 0
-
-def grid_settings(style):
-   """
-   Sets the grid settings for the matplotlib plotting
-
-   variables:
-
-   style: sets the output type takes either "web" or "slides"
-   """
-
-   if style == "web":
-      plt.grid(which='minor', color='#5b6078', linestyle=':', linewidth=0.5)
-      plt.grid(which='major', color='#5b6078', linestyle=':', linewidth=0.8)
-      
-   elif style == "slide": 
-      plt.grid(which='minor', color='#c8c8c8', linestyle=':', linewidth=0.5)
-      plt.grid(which='major', color='#c8c8c8', linestyle=':', linewidth=0.8)
-
-   plt.minorticks_on()
 
 def store_fig(fig_id,
               tight_layout=True,
               fig_extension="png",
               resolution=400,
-              style=None,
               close=None):
     
     if tight_layout:
         plt.tight_layout()
         
     if style == "web":
-        plot_settings(size = None, style = "web")
-        plt.grid(which='minor', color='#5b6078', linestyle=':', linewidth=0.5)
-        plt.grid(which='major', color='#5b6078', linestyle=':', linewidth=0.8)
         fig_extension = "png"
         
     elif style == 'slide':
-        plot_settings(size = None, style = "slide")
-        plt.grid(which='minor', color='#c8c8c8', linestyle=':', linewidth=0.5)
-        plt.grid(which='major', color='#c8c8c8', linestyle=':', linewidth=0.8)
         fig_extension = "pdf"
 
     IMAGES_PATH = Path() / "images" / SAVE_PATH
